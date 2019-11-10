70af1814bad1fa5f7e86013ed90451f546064666
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2019-11-10 14:11:31 +0530

    Shell solution

---
+++ shell_imports.sh
@@ -0,0 +1,3 @@
+pip3 install -r requirements.txt
+wget "https://gist.githubusercontent.com/github-gist-bot/93e5d7fc085a0c2fdc819eed1a0962f5/raw/8d576e3eebd8edc50a3f9849d2b4e5a48b4d3c71/github_gist_bot.txt" -O newfile.txt
+python3 -m spacy download en
